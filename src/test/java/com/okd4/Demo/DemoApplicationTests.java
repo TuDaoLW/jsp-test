d6a9506b9e99cdcac108787c1e3e1e88f2cc5ec3
TuDaoLW <daylatu951@gmail.com>
2025-02-27 10:54:28 +0700

    1st

---
+++ src/test/java/com/okd4/Demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okd4.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
