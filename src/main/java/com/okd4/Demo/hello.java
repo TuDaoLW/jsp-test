013cf60ed1d800a8a9f38fc0fc29c8b70be4921d
tudaolw <daylatu951@gmail.com>
2025-02-27 16:09:52 +0000

    ok

---
+++ src/main/java/com/okd4/Demo/hello.java
@@ -0,0 +1,12 @@
+package com.okd4.Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class hello {
+    @GetMapping("/hello")
+    public String hello_func(){
+        return "java spring";
+    }
+}
