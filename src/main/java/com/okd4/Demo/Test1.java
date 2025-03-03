0004ffa5024f2e6dc526945739ba729ad9c18f4c
TuDaoLW <daylatu951@gmail.com>
2025-03-04 00:04:19 +0700

    ok

--- src/main/java/com/okd4/Demo/Test1.java
+++ src/main/java/com/okd4/Demo/Test1.java
@@ -3,10 +3,10 @@ package com.okd4.Demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
-public class hello {
-    @GetMapping("/hello")
-    public String hello_func(){
-        return "java spring";
-    }
+@RestController
+public class Test1 {
+	@GetMapping("/hello")
+	public String hello_func() {
+		return "hello";
+	}
 }
