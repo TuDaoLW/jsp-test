542bcb7be30b9f7caf8a54f945d952483027db66
TuDaoLW <160880128+TuDaoLW@users.noreply.github.com>
2025-03-05 18:22:36 +0700

    Update Test1.java

--- src/main/java/com/okd4/Demo/Test1.java
+++ src/main/java/com/okd4/Demo/Test1.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test1 {
 	@GetMapping("/hello")
 	public String hello_func() {
-		return "hello v1 check img 2";
+		return "hello v2 check img 2";
 	}
 }
